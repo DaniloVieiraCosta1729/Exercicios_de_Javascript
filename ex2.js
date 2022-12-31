3838cdf6203bbd4b6210542e5684b2ca2f0e79cc
Danilo Vieira Costa <danilo.11235@outlook.com>
2022-12-31 04:11:14 -0300

    Segundo exercício.

---
+++ ex2.js
@@ -0,0 +1,8 @@
+// Exercício 2
+
+let a = c = 10, b = 20;
+
+console.clear(); console.log("==================exercício 2==================");console.log("O valor da variável C é:");
+console.log(c);
+a = b;
+console.log(`Agora a variável A é igual a ${a} e a variável B continua igual a ${b}.`);
